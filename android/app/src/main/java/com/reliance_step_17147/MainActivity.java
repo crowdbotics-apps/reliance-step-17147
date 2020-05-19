bcb5fe0407568c0f6c7cbc0ead598830e1806616
Crowdbotics <team@crowdbotics.com>
2020-05-19 15:22:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/reliance_step_17147/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reliance_step_17147;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "reliance_step_17147";
+    }
+}
